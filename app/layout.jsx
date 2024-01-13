52de699da85b7c55cc5820fdc3965c97e9949e85
Shivam <shekharshivam51@gmail.com>
2024-01-13 18:21:43 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import Navbar from '@/components/Navbar'
+import './globals.css'
+import { Metadata } from 'next';
+import { Inter, Space_Grotesk } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({ 
+  subsets: ['latin'], 
+  weight: ['300', '400', '500', '600', '700']
+})
+
+export const metadata = {
+  title: 'Pricewise',
+  description: 'Track product prices effortlessly and save money on your online shopping.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10xl mx-auto">
+          <Navbar />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
